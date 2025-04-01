9249fe126f93df45318e1f03dc2022c553ba8b3d
jin-act <61611967+jin-act@users.noreply.github.com>
2025-04-01 12:52:45 +0900

    bash_operator dag 오류 수정

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -26,5 +26,7 @@ with DAG(
     bash_t3 = BashOperator(
         task_id='bash_t3',
         bash_command='env | grep HOSTNAME && cat /etc/hostname'
+    )
     
     bash_t1 >> bash_t2 >>bash_t3
+    
