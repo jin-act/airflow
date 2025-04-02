d5a0675670bbec48878c7a40f7a3430eaf0d6645
jin-act <61611967+jin-act@users.noreply.github.com>
2025-04-02 18:05:56 +0900

    실습 fruit_sel 실행하는 dag

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -4,20 +4,19 @@ import pendulum
 from airflow.operators.bash import BashOperator
 
 with DAG(
-    dag_id="dags_bash_operator",
-    schedule="0 0 * * *",
+    dag_id="dags_bash_select_fruit",
+    schedule="10 0 * * 6#1",
     start_date=pendulum.datetime(2025, 1, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
     
     select_t1 = BashOperator(
         task_id="select_t1",
-        bash_command="$ORANGE",
-    )
+        bash_command="/opt/airflow/plugins/shell/select_fruit.sh ORANGE",    )
        
     select_t2 = BashOperator(
         task_id="select_t2",
-        bash_command="$AVOCADO",
+        bash_command="/opt/airflow/plugins/shell/select_fruit.sh AVOCADO",
     )
     
     select_t1 >> select_t2
