69eb1908539fa8e3bf9a2bac21d399ba835a1c8c
jin-act <61611967+jin-act@users.noreply.github.com>
2025-04-08 20:48:54 +0900

    airflow 변경 된 예시의 방식 적용

---
+++ dags/dags_python_task_decorator2.py
@@ -0,0 +1,19 @@
+from airflow.models.dag import DAG
+import pendulum
+from pprint import pprint
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    dag_id="dags_python_task_decorator",
+    schedule="0 2 * * 1",
+    start_date=pendulum.datetime(2025, 1, 1, tz="Asia/Seoul"),
+    catchup=False,
+):
+    
+    # [START howto_operator_python]
+    def print_context(some_input):
+        print(some_input)
+
+    python_task_1 = PythonOperator(task_id='python_task_1',python_callable=print_context('task_decorator 실행'))
+
+    python_task_1
