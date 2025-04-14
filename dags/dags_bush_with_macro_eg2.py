4931146e7dce3b045e0ffa07cfc4d402e717051c
jin-act <61611967+jin-act@users.noreply.github.com>
2025-04-14 21:00:54 +0900

    오류 수정

--- dags/dags_bush_with_macro_eg2.py
+++ dags/dags_bush_with_macro_eg2.py
@@ -4,7 +4,7 @@ from airflow.operators.bash import BashOperator
 
 with DAG(
     dag_id="dags_bush_with_macro_eg2",
-    schedule="10 0 L * 6#2",
+    schedule="10 0 * * 6#2",
     start_date=pendulum.datetime(2025, 4, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
@@ -14,7 +14,7 @@ with DAG(
         env={'START_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=-19)) | ds }}',
              'END_DATE' : '{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=-14)) | ds }}'
         },
-        bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: &END_DATE"'
+        bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
     )
 
     bash_task_1
