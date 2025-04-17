7d2d71d758361303885fb52302400a66e85f19e4
jin-act <61611967+jin-act@users.noreply.github.com>
2025-04-17 16:01:41 +0900

    에러 모르겠지만, 변동 있어서 커밋

--- dags/dags_bash_with_vairable.py
+++ dags/dags_bash_with_vairable.py
@@ -9,7 +9,7 @@ with DAG(
     start_date=pendulum.datetime(2025, 4, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
-    var_value = Variable.get("semple_key")
+    var_value = Variable.get("sample_key")
 
     bash_var_1 = BashOperator(
         task_id='bash_var_1',
