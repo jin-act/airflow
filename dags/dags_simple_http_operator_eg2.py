07a46abbd7f1b7473962b9f8e027a2e4d4d0c0eb
jin-act <61611967+jin-act@users.noreply.github.com>
2025-04-25 21:06:15 +0900

    dag id

--- dags/dags_simple_http_operator_eg2.py
+++ dags/dags_simple_http_operator_eg2.py
@@ -6,7 +6,7 @@ from airflow.decorators import task
 import pendulum
 
 with DAG(
-    dag_id='dags_simple_http_operator',
+    dag_id='dags_simple_http_operator_eg2',
     start_date=pendulum.datetime(2025, 4, 1, tz='Asia/Seoul'),
     catchup=False,
     schedule=None
